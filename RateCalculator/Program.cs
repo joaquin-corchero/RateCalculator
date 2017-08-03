247c4acfb150bac2a685fe3d6bcc8689d0f71b4e
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-08-03 20:31:36 +0100

    Implementation of the validation for no arguments

--- RateCalculator/Program.cs
+++ RateCalculator/Program.cs
@@ -11,9 +11,27 @@ namespace RateCalculator
     {
         static IInputValidator _inputValidator;
 
+        static void SetupDependencies()
+        {
+            _inputValidator = new InputValidator();
+        }
+
         static void Main(string[] args)
         {
-            
+            SetupDependencies();
+            var validationResult = _inputValidator.Validate(args);
+            if (!validationResult.IsValid)
+            {
+                Console.WriteLine(validationResult.Message);
+                PressKeyAndWait();
+                return;
+            }
+        }
+
+        static void PressKeyAndWait()
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
     }
 }
